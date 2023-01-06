73422c046db80fb27f118b607bc272b9c7066b1b
Adrian Drummond <adrian.drummond@gmail.com>
2023-01-06 12:39:21 -0800

    adding original file

---
+++ get_value.py
@@ -0,0 +1,11 @@
+player_hand = [[8, "spaids"], [10, "spaids"]]
+
+def get_value(hand):
+  for card in hand:
+    for item in card:
+      if item == int():
+        item += item
+        return item
+
+get_value(player_hand)
+print(player_hand)
